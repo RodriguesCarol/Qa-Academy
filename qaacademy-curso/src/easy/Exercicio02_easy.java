7ff8a04b916b9f556460f5234920f503a6de087f
carolinemagalhaes <carolinemagalhaes@touchtec.com.br>
2024-01-23 19:11:46 -0300

    teste modificação

--- qaacademy-curso/src/easy/Exercicio02_easy.java
+++ qaacademy-curso/src/easy/Exercicio02_easy.java
@@ -6,7 +6,7 @@ public class Exercicio02_easy                                                   
     public static void main(String[] args) {
     String  palavraDigitada;
     palavraDigitada = JOptionPane.showInputDialog("Digite um número inteiro");
-    System.out.println("O usuário digitou: " + palavraDigitada);
+    System.out.println("O usuário digitou isso: " + palavraDigitada);
 
         
     }
