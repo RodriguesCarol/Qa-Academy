31814de245dfd12769474d1943a0fbdba2dee3f0
RodriguesCarol <rodriguesdemagalhaes@gmail.com>
2024-01-25 17:43:33 -0300

    Refatoração do exercicio 04 - Easy

--- qaacademy-curso/src/easy/EXecucao.java
+++ qaacademy-curso/src/easy/EXecucao.java
@@ -9,17 +9,21 @@ public class EXecucao {
         // System.out.println(palavra);
 
 
-        String palavra1 = JOptionPane.showInputDialog("DIGITE UM VALOR");
-        String palavra2 = JOptionPane.showInputDialog("DIGITE UM VALOR");
-        Exercicio03_easy exercicio03_easy = new Exercicio03_easy();
-        String valorTrocado = (exercicio03_easy.retornaPalavrasTrocadas(palavra1, palavra2));
-        System.out.println(valorTrocado);
+        // String palavra1 = JOptionPane.showInputDialog("DIGITE UM VALOR");
+        // String palavra2 = JOptionPane.showInputDialog("DIGITE UM VALOR");
+        // Exercicio03_easy exercicio03_easy = new Exercicio03_easy();////Instanciando a classe Exercicio 03 easy
+        // String valorTrocado = (exercicio03_easy.retornaPalavrasTrocadas(palavra1, palavra2)); /// Fazendo a chamada e gravando na váriavel valorTrocado
+        // System.out.println(valorTrocado);
 
-        // Exercicio04_easy exercicio04_easy = new Exercicio04_easy();
-        // Integer resultado (exercicio04_easy.retornoDobro(20));
-        // System.out.println(resultado);
+    //    String numeroDigitadoUsuario = JOptionPane.showInputDialog("Digite um número");
+    //    String numeroDigitado = Integer.parseInt(numeroDigitadoUsuario);
+    //    String resultado = numeroDigitado *2;
+
+      String numeroDigitadoUsuario = JOptionPane.showInputDialog("Digite um número");
+      Integer numeroDigitado = Integer.parseInt(numeroDigitadoUsuario);
+      Exercicio04_easy exercicio04_easy = new Exercicio04_easy();
+      System.out.println("O dobro de " + numeroDigitado+ " é " + exercicio04_easy.calculaDobro(numeroDigitado));
 
     }
 
-    
 }
