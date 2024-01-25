78238813d4ce19abf28527e5b9c8305a9fc8c204
RodriguesCarol <rodriguesdemagalhaes@gmail.com>
2024-01-25 17:15:34 -0300

    Correção da Refatoração do exercicio 03

--- qaacademy-curso/src/easy/EXecucao.java
+++ qaacademy-curso/src/easy/EXecucao.java
@@ -1,14 +1,23 @@
 package easy;
 
+import javax.swing.JOptionPane;
+
 public class EXecucao {
     public static void main(String[] args) {
-        Exercicio02_easy exercicio02_easy = new Exercicio02_easy();
-        String palavra = exercicio02_easy.retornaPalavra("Carol");
-        System.out.println(palavra);
+        // Exercicio02_easy exercicio02_easy = new Exercicio02_easy();
+        // String palavra = exercicio02_easy.retornaPalavra("Carol");
+        // System.out.println(palavra);
 
+
+        String palavra1 = JOptionPane.showInputDialog("DIGITE UM VALOR");
+        String palavra2 = JOptionPane.showInputDialog("DIGITE UM VALOR");
         Exercicio03_easy exercicio03_easy = new Exercicio03_easy();
-        String palavras = exercicio03_easy.retornaPalavras("Carol", "Rodrigues");
-        System.out.println(palavras);
+        String valorTrocado = (exercicio03_easy.retornaPalavrasTrocadas(palavra1, palavra2));
+        System.out.println(valorTrocado);
+
+        // Exercicio04_easy exercicio04_easy = new Exercicio04_easy();
+        // Integer resultado (exercicio04_easy.retornoDobro(20));
+        // System.out.println(resultado);
 
     }
 
--- qaacademy-curso/src/easy/Exercicio03_easy.java
+++ qaacademy-curso/src/easy/Exercicio03_easy.java
@@ -2,19 +2,19 @@ package easy;
 //import javax.swing.JOptionPane;
 
 public class Exercicio03_easy {//inicio algoritimo
-    public String retornaPalavras (String palavra1, String palavra2){
-        return "A primeira palavra digitada é  "+ palavra1 + " " + "e a segunda é "+ palavra2;
+    public String retornaPalavrasTrocadas (String palavra1, String palavra2){
+        return "O primeiro valor digitado é: "+ palavra2 + "\n" + "O segundo vaor digitado é: "+ palavra1;
     }
 
 }//fim algoritimo
-// ///DECLARANDO VARIAVEIS
-    // STATIC STRING PRIMEIROVALOR; 
-    // STATIC STRING SEGUNDOVALOR;
-    // PUBLIC STATIC VOID MAIN(STRING[] ARGS) { //INICIO MÉTODO "MAESTRO"
-    //     PRIMEIROVALOR = JOPTIONPANE.SHOWINPUTDIALOG("DIGITE UM VALOR");
-    //     SEGUNDOVALOR = JOPTIONPANE.SHOWINPUTDIALOG("DIGITE UM VALOR");
-    // SYSTEM.OUT.PRINTLN("O PRIMEIRO VALOR DIGITADO É :"+ PRIMEIROVALOR);
-    // SYSTEM.OUT.PRINTLN ("O SEGUNDO VALOR                    DIGITADO É :"+ SEGUNDOVALOR);
-    // }//FIM METODO MAESTRO
+//  ///DECLARANDO VARIAVEIS
+//  STATIC STRING PRIMEIROVALOR;
+//  STATIC STRING SEGUNDOVALOR;
+//  PUBLIC STATIC VOID MAIN(STRING[] ARGS) { //INICIO MÉTODO "MAESTRO"
+//  PRIMEIROVALOR = JOPTIONPANE.SHOWINPUTDIALOG("DIGITE UM VALOR");
+//  SEGUNDOVALOR = JOPTIONPANE.SHOWINPUTDIALOG("DIGITE UM VALOR");
+//  SYSTEM.OUT.PRINTLN("O PRIMEIRO VALOR DIGITADO É :"+ PRIMEIROVALOR);
+//  SYSTEM.OUT.PRINTLN ("O SEGUNDO VALOR DIGITADO É :"+ SEGUNDOVALOR);
+//  }//FIM METODO MAESTRO
     
     
