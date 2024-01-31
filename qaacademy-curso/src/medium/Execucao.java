61c35d718dd7c16607b4bbb4dac644da64bdedec
RodriguesCarol <rodriguesdemagalhaes@gmail.com>
2024-01-31 20:43:42 -0300

    Refatoração exercicio 04 - Medium

--- qaacademy-curso/src/medium/Execucao.java
+++ qaacademy-curso/src/medium/Execucao.java
@@ -9,14 +9,17 @@ public class Execucao {
         // Exercicio01_medium exercicio01_medium = new Exercicio01_medium();
         // exercicio01_medium.retornaParImpar();
 
-        String valorInvestidoStg = JOptionPane.showInputDialog("Qual o valor investido?");
-        double valorInvestido = Double.parseDouble(valorInvestidoStg);
-        Exercici02_medium exercici02_medium =new Exercici02_medium();
-        double valorTotal = exercici02_medium.valorInvestidoJurosComposto(valorInvestido);
-        double valorJuros = exercici02_medium.valorJurosComposto(valorTotal, valorInvestido);
-        System.out.println("O valor investido é: " + valorInvestido);
-        System.out.println("O valor do juros é " + valorJuros);
-        System.out.println("O valor acumulado é: " + valorTotal);
+        // String valorInvestidoStg = JOptionPane.showInputDialog("Qual o valor investido?");
+        // double valorInvestido = Double.parseDouble(valorInvestidoStg);
+        // Exercici02_medium exercici02_medium =new Exercici02_medium();
+        // double valorTotal = exercici02_medium.valorInvestidoJurosComposto(valorInvestido);
+        // double valorJuros = exercici02_medium.valorJurosComposto(valorTotal, valorInvestido);
+        // System.out.println("O valor investido é: " + valorInvestido);
+        // System.out.println("O valor do juros é " + valorJuros);
+        // System.out.println("O valor acumulado é: " + valorTotal);
+
+        Exercicio04_medium exercicio04_medium = new Exercicio04_medium();
+        exercicio04_medium.resultadoMultiplicacao();
     }
     
 }
