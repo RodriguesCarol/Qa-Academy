24b3dc4253d41e79fa787bde15e17d6938d34793
RodriguesCarol <rodriguesdemagalhaes@gmail.com>
2024-01-31 20:04:17 -0300

    Refatoração exercicío 01-Medium

---
+++ qaacademy-curso/src/medium/Execucao.java
@@ -0,0 +1,13 @@
+package medium;
+
+public class Execucao {
+
+    public static void main(String[] args) {
+
+        Exercicio01_medium exercicio01_medium = new Exercicio01_medium();
+        exercicio01_medium.retornaParImpar();
+
+        
+    }
+    
+}
